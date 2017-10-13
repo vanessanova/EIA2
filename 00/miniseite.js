411e4ec2359569f46cbce15b3715a779947c27b9
NB-Admin <NB-Admin@Princessa.eduroam.hs-furtwangen.de>
2017-10-13 18:53:13 +0200

    .

---
+++ 00/miniseite.js
@@ -0,0 +1,11 @@
+function Aloha() {
+    var button;
+    var person = prompt("Whats your Name?", "");
+    if (person == null || person == "") {
+    }
+    else {
+        button = "Aloha " + person + " !  Whats up?";
+    }
+    document.getElementById("button").innerHTML = button;
+}
+//# sourceMappingURL=miniseite.js.map
